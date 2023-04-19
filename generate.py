04136e4e87915cad40199d26f671ec76b1862917
Angela-CMU <chienyul@andrew.cmu.edu>
2023-04-19 19:40:32 -0400

    add gps and make sure data make sense and reasonable

--- generate.py
+++ generate.py
@@ -59,16 +59,61 @@ simulation_data_template = {
 }
 
 
+# "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"
+def generate_gps(num_positions):
+    # print("called")
+    gps_data = []
+    randomNum = random.randint(0, 30)
+    for i in range(num_positions):
+        gps = "$GPGGA,"
+        gps += str(123500 + i + randomNum)
+        gps += ","
+        gps += str(4807 + i * 0.02 + randomNum)
+        gps += ",N,"
+        gps += str(11131 + i * 0.01 + randomNum)
+        gps += ",E,1,08,0.9,545.4,M,46.9,M,,*46"
+        gps_data.append(gps)
+    # print(gps_data)
+    return gps_data
+
+
 def generate_xyz(num_positions):
     positions = []
-    for _ in range(num_positions):
+    X = round(random.uniform(0, 1), 2)
+    Y = round(random.uniform(0, 1), 2)
+    Z = round(random.uniform(0, 1), 2)
+    for i in range(num_positions):
         position = {
-            "X": round(random.uniform(0, 1), 2),
-            "Y": round(random.uniform(0, 1), 2),
-            "Z": round(random.uniform(0, 1), 2),
+            "X": round ( X + i*0.01 + round(random.uniform(-0.05, 0.05), 2), 2),
+            "Y": round ( Y + i*0.01 + round(random.uniform(-0.05, 0.05), 2), 2),
+            "Z": round ( Z + i*0.01 + round(random.uniform(-0.05, 0.05), 2), 2),
         }
         positions.append(position)
     return positions
+
+
+def generate_num(num_positions, a, b):
+    positions = []
+    num = random.randint(a, b)
+    nums = []
+    nums.append(num)
+    for i in range(num_positions):
+        num = num + random.randint(-2, 2)
+        nums.append(num)
+    return nums
+
+
+def generate_status(num_positions):
+    if num_positions <= 0: 
+      return []
+    count = random.randint(2, 5)
+    count = min(count, num_positions)
+    num = random.randint(0, 1)
+    value = []
+    for _ in range(count):
+      value.append(num)
+    return value + generate_status(num_positions - count)
+
 
 
 def generate_simulation_data(num_simulations=10):
@@ -81,36 +126,38 @@ def generate_simulation_data(num_simulations=10):
             simulation["StartTime"], "%Y-%m-%dT%H:%M:%S")
         end_time = datetime.strptime(
             simulation["EndTime"], "%Y-%m-%dT%H:%M:%S")
+        
+        results = generate_gps(number_data)
+        simulation["gps"] = results
 
         for j in range(number_data):
             time = start_time + \
                 timedelta(seconds=random.randint(
                     0, int((end_time - start_time).total_seconds())))
             time_str = time.strftime("%Y-%m-%dT%H:%M:%S")
-            simulation["Speed"]["value"].append(random.randint(0, 100))
+
+            simulation["Speed"]["value"] = generate_num(number_data, 0, 100)
             simulation["Speed"]["time"].append(time_str)
 
-            simulation["CarStatus"]["value"].append(random.randint(0, 1))
+            simulation["CarStatus"]["value"] = generate_status(number_data)
             simulation["CarStatus"]["time"].append(time_str)
 
-            simulation["Accel"]["value"].append(random.randint(-10, 10))
+            simulation["Accel"]["value"] = generate_num(number_data, -10, 10)
             simulation["Accel"]["time"].append(time_str)
 
-            simulation["Direction"]["value"].append(random.randint(-10, 10))
+            simulation["Direction"]["value"] = generate_num(number_data, -10, 10)
             simulation["Direction"]["time"].append(time_str)
 
-            simulation["ServoAngle"]["value"].append(random.randint(-10, 10))
+            simulation["ServoAngle"]["value"] = generate_num(number_data, -10, 10)
             simulation["ServoAngle"]["time"].append(time_str)
 
-            simulation["EngineTemperature"]["value"].append(
-                round(random.uniform(-15, 15), 1))
+            simulation["EngineTemperature"]["value"] = generate_num(number_data, -15, 15)
             simulation["EngineTemperature"]["time"].append(time_str)
 
-            simulation["RPM"]["value"].append(random.randint(-10, 10))
+            simulation["RPM"]["value"] = generate_num(number_data, -10, 10)
             simulation["RPM"]["time"].append(time_str)
 
             simulation["Position"]["value"] = generate_xyz(number_data)
-            # simulation["Position"]["value"].append(random.randint(-10, 10))
             simulation["Position"]["time"].append(time_str)
 
             simulation["Yaw"]["value"] = generate_xyz(number_data)
@@ -132,8 +179,10 @@ def generate_simulation_data(num_simulations=10):
 
 num_simulations_test = 5
 
+
 for index in range(num_simulations_test):
     simulation_data = generate_simulation_data(1)
     # Save the data into a JSON file
     with open("./" + str(index), "w") as outfile:
         json.dump(simulation_data, outfile, indent=2)
+        
